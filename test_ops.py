343a069889d2dc1b76de75ad310a3407abda98fe
Ujjwal Sharma <65191782+UjjwalSharma00@users.noreply.github.com>
2024-06-10 23:15:06 +0530

    Create test_ops.py

---
+++ test_ops.py
@@ -0,0 +1,23 @@
+from ops import *
+import numpy as np
+
+x = np.random.randint(0, 100)
+y = np.random.randint(0, 100)
+
+print(f'x = {x}, y = {y}')
+
+def test_add():
+    assert add(2,3) == 5
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+
+def test_multiply():
+    assert multiply(3, 3) == 9
+
+def test_divide():
+    assert divide(10,5) == 2
+
+print ('All tests pass!')
+
+print ('Testing done')
